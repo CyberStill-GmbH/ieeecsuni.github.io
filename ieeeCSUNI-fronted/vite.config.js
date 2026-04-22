55ecaad190ce41a9c8f6a3a0c2fe0809ec610513
CyberStill-GmbH <cesar.guevara.s@uni.pe>
2026-04-22 16:38:10 -0500

    feat: añadir configuración de proxy para autenticación y rutas de API en Vite

--- ieeeCSUNI-fronted/vite.config.js
+++ ieeeCSUNI-fronted/vite.config.js
@@ -3,10 +3,45 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': '/src',
     },
   },
+
   base: '/ieeecsuni.github.io/',
+
+  server: {
+    port: 5173,
+    strictPort: true,
+
+    proxy: {
+      '/ieeecsuni.github.io/sanctum': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ieeecsuni.github.io/, '') // Quita el prefijo para Laravel
+      },
+      '/ieeecsuni.github.io/login': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ieeecsuni.github.io/, '')
+      },
+      '/ieeecsuni.github.io/register': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ieeecsuni.github.io/, '')
+      },
+      '/ieeecsuni.github.io/logout': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ieeecsuni.github.io/, '')
+      },
+      '/ieeecsuni.github.io/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ieeecsuni.github.io/, '')
+      },
+    },
+  },
 })
