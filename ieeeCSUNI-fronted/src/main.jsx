620fbc85b98437ab9136e22401323fe08612ade4
CyberStill-GmbH <cesar.guevara.s@uni.pe>
2026-04-29 18:34:06 -0500

    fix: remove unnecessary Outlet import and update BrowserRouter basename

--- ieeeCSUNI-fronted/src/main.jsx
+++ ieeeCSUNI-fronted/src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Outlet } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ieeecsuni.github.io/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
