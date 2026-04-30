07b57e6b845814124009d4ba93c39a6e733f37d3
CyberStill-GmbH <cesar.guevara.s@uni.pe>
2026-04-30 18:39:14 -0500

    feat: update CORS configuration to include CSRF cookie path and clean up allowed origins

--- ieeeCSUNI-backend/config/cors.php
+++ ieeeCSUNI-backend/config/cors.php
@@ -1,11 +1,23 @@
 <?php
 
 return [
-    'paths'           => ['api/*'],
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'https://ieeecsuni.vercel.app/',  
-    ],
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://ieeecsuni.vercel.app',
+    ],
+
+    'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
 ];
