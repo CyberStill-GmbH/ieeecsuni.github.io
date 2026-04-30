2647c2171ca2929bfb8b59e11bb6d1c7ec3224d8
CyberStill-GmbH <cesar.guevara.s@uni.pe>
2026-04-30 18:36:25 -0500

    feat: add CORS middleware configuration and update API routes for OPTIONS request handling

--- ieeeCSUNI-backend/bootstrap/app.php
+++ ieeeCSUNI-backend/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withMiddleware(function ($middleware) {
+    $middleware->append(HandleCors::class);
+    })
+    
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(prepend: [
              \Illuminate\Http\Middleware\HandleCors::class,
--- ieeeCSUNI-backend/routes/api.php
+++ ieeeCSUNI-backend/routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/contact', [ContactController::class, 'store'])
     ->middleware('throttle:10,1');
+
+Route::options('/{any}', function () {
+    return response()->json([], 200);
+})->where('any', '.*');
